8b6b93d019a287d0ba608db5abf8bc5b39681b2f
Houda-DE <houdadebza8@gmail.com>
2022-10-28 13:20:01 +0100

    ...

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post , Patch , Delete, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { ProductDto } from './dto';
 import { ProductService } from './product.service';
 
@@ -11,5 +12,25 @@ export class ProductController {
     addProduct (@Body() dto : ProductDto) {
         console.log(dto)
         return this.productService.addProduct(dto)
+    }
+
+    @Get('products')
+    findAllProducts(){
+        return this.productService.findAllProducts()
+    }
+
+    @Get(':id')
+    findOneProduct(@Body() dto : ProductDto){
+        return this.productService.findOneProduct(dto)
+    }
+
+    @Patch(':id')
+    modifyProduct(@Body() dto : ProductDto , @Req() req : Request){
+        return this.productService.modifyProduct(dto , req)
+    }
+
+    @Delete(':id')
+    deleteProduct(@Body() dto : ProductDto){
+        return this.productService.deleteProduct(dto)
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { stringify } from 'querystring';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductDto } from './dto';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+import { Request } from 'express';
 
 @Injectable()
 export class ProductService {
+
     constructor (private prisma : PrismaService){}
 
-    async addProduct (dto : ProductDto) {
-        
+    async addProduct (dto : ProductDto) { 
         try {
             const product = await this.prisma.product.create({
                 data : {
@@ -21,7 +23,57 @@ export class ProductService {
             })
         }
         catch(error) {
+            if (error instanceof PrismaClientKnownRequestError){
+                //P2002 sends to duplicate users
+                if(error.code === 'P2002'){
+                    throw new ForbiddenException('Credential taken')
+                }
+            }
             throw error
         }
     }
+
+    async findAllProducts(){
+        return await this.prisma.product.findMany()
+    }
+
+    async findOneProduct(dto : ProductDto){
+       try {
+            return await this.prisma.product.findUniqueOrThrow({
+                where : {
+                    name : dto.name
+                },
+                select : {
+                    name : true,
+                    id : true,
+                    description : true,
+                    imageUrl : true,
+                    prix : true,
+                    quantity : true,
+                }
+            })
+       }
+       catch(error){
+            throw error
+       }
+    }
+
+    async modifyProduct(dto : ProductDto , req : Request){
+        try{
+            return this.prisma.product
+        }
+        catch (error) {
+            throw error
+        }
+    }
+
+    async deleteProduct(dto : ProductDto){
+        try{
+
+        }
+        catch (error) {
+            throw error
+        }
+    }
+
 }
